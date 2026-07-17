50def112353b4145ed604f8bfea3aa971e9dc4c7
bread-over-key <possiblemishin@gmail.com>
2026-07-16 21:27:03 -0400

    updated edit

--- components/edit-goal.tsx
+++ components/edit-goal.tsx
@@ -22,24 +22,53 @@ export default function EditGoal(props: EditGoalProps) {
 		(param: K, value: GoalDto[K]) {
 		let newGoal: GoalDto = { ...goal };
 		newGoal[param] = value
+		console.dir(newGoal)
 		setGoal(newGoal)
-
+		console.dir(newGoal)
 	}
 
 	function handleUpdate() {
 		const { id, ...newGoal } = goal
 		props.onUpdate(goal.id, newGoal)
+		console.dir(newGoal)
 	}
 
-	return <Stack>
+	return <Stack spacing={2} >
 		<TextField
 			label={"name"}
 			value={goal.name}
 			onChange={e => updateParameter("name", e.target.value)} />
-		<TextField />
-		<TextField />
-		<TextField />
-		<Checkbox />
+		<TextField
+			label={"water duration"}
+			type="number"
+			value={goal.waterDuration}
+			onChange={
+				e => updateParameter(
+					"waterDuration",
+					Number.parseInt(e.target.value)
+				)
+			}
+		/>
+		<Typography>archived</Typography>
+		<Checkbox
+			checked={goal.archived}
+			onChange={
+				e => updateParameter(
+					"archived",
+					e.target.checked
+				)
+			}
+		/>
+		<Typography>milestone enabled`</Typography>
+		<Checkbox
+			checked={goal.milestoneEnabled}
+			onChange={
+				e => updateParameter(
+					"milestoneEnabled",
+					e.target.checked
+				)
+			}
+		/>
 		<Button onClick={handleUpdate}>Update</Button>
 	</Stack>
 }
